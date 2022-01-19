76d979ebb9bd4fc3427b5e3142f06d5110e073e0
Sujal <shahc9437@gmail.com>
2022-01-19 12:49:48 +0530

    ✅ Copy button added to codeblock

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
   changeBackground();
   setInterval(changeBackground, 2000);
 });
+
+function copyCode() {
+  navigator.clipboard.writeText(
+    "import React from 'react';  import {Text, View} from 'react-native';  import {Header} from './Header';  import {heading} from './Typography';    const WelcomeScreen = () => (    <View>      <Header title=\"Welcome to React Native\"/>      <Text style={heading}>Step One</Text>      <Text>        Edit App.js to change this screen and turn it        into your app.      </Text>      <Text style={heading}>See Your Changes</Text>      <Text>        Press Cmd + R inside the simulator to reload        your app’s code.      </Text>      <Text style={heading}>Debug</Text>      <Text>        Press Cmd + M or Shake your device to open the        React Native Debug Menu.      </Text>      <Text style={heading}>Learn</Text>      <Text>        Read the docs to discover what to do next:      </Text></View>);"
+  );
+  document.getElementsByClassName('copy-button')[0].innerText = 'Copied';
+  setTimeout(() => {
+    document.getElementsByClassName('copy-button')[0].innerText = 'Copy';
+  }, 2000);
+}
